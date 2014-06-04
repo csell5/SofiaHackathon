53cb570e24d2455bbc93d669310d356d554fe923
Clark Sell <clark.sell@telerik.com>
2014-06-04 04:58:44 -0500

    wired up button on home view

--- Rey-vs-Brandon/scripts/home.js
+++ Rey-vs-Brandon/scripts/home.js
@@ -3,6 +3,8 @@
         app = global.app = global.app || {};
 
     HomeViewModel = kendo.data.ObservableObject.extend({
+        trashTalkMessage: "", 
+        trashTalkUrl: "",
         _isLoading: false,
 
         onNavigateHome: function () {
@@ -18,16 +20,31 @@
             } else {
                 kendo.mobile.application.hideLoading();
             }
-        }
+        },
+        
+        sendMessage: function () {
+            console.log("SendMessage func was called");
+            
+              var that = this,
+                trashTalkMessage = that.get("trashTalkMessage").trim(),
+                trashTalkUrl = that.get("trashTalkUrl").trim();
+
+            console.log (trashTalkMessage);
+            console.log(trashTalkUrl);
+            
+            if (trashTalkMessage === "") {
+                navigator.notification.alert("Y U NO Talk Trash!",
+                    function () { }, "Y U NO Talk Some Trash Homie", 'OK');
+                return;
+            }
+            
+            // send push notification
+        },
 
     });
 
     app.homeService = {
-		sendMessage: function () {
-
-        },
-        
-        viewModel: new HomeViewModel()
+		viewModel: new HomeViewModel()
     };
 }
 )(window);
